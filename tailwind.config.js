04aace0ac1d24c7607a080c6cefd362d570b49d0
Shahbaz Ali <shahbazali5097855@gmail.com>
2024-02-16 09:51:41 +0500

    All Files Added from Local to Remote

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./favIconGrabber\\index.html/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
